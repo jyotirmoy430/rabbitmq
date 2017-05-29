a5090dcadf4cfe31509132f1f17e716e759595b1
Jyotirmoy Biswas <jyoti@cefalo.com>
2017-05-29 11:20:48 +0600

    Added master server to check

--- php/new_task.php
+++ php/new_task.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('54.93.197.174', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
 
--- php/worker.php
+++ php/worker.php
@@ -3,10 +3,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('54.93.197.174', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('ticket_queue', false, true, false, false);
+$channel->queue_declare('task_queue', false, true, false, false);
 
 echo ' [*] Waiting for messages. To exit pressss CTRL+C', "\n";
 
@@ -17,13 +17,13 @@ $callback = function($msg){
 
   echo " [x] Done", "\n";
 
-    postimportlog('file', __LINE__, $msg->body);
+   // postimportlog('file', __LINE__, $msg->body);
 
   $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
 };
 
 $channel->basic_qos(null, 1, null);
-$channel->basic_consume('ticket_queue', '', false, false, false, false, $callback);
+$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
 
 while(count($channel->callbacks)) {
     $channel->wait();
